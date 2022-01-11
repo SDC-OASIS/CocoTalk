a857b42be808660e914633e141746ca866f1526e
권희은/SGS DevCamp <heunkwon@smilegate.com>
2022-01-11 21:38:40 +0900

    build skeleton for FE

---
+++ cocotalk/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": "off",
+    "prettier/prettier": ['error', {
+      "semi": true,
+      "useTabs": true,
+      "tabWidth": 2,
+      "trailingComma": "all",
+      "printWidth": 120,
+      "endOfLine": "auto"
+    }]
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ cocotalk/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld class="dd" msg="Welcome to Your Vue.js App" />
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+	name: "Home",
+	components: {
+		HelloWorld,
+	},
+};
+</script>
+<style scoped>
+.dd {
+	display: block;
+	justify-content: center;
+}
+</style>
