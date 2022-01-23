8f7211a5b38d61281afec961d09dcc2db8177851
황종훈 <ybell1028@gmail.com>
2022-01-23 23:44:23 +0900

    feat : 친구 조회 기능 추가
    - 친구 추가 Request 모델 변경
    - UserResponse 삭제

--- src/main/java/com/cocotalk/user/service/FriendService.java
+++ src/main/java/com/cocotalk/user/service/FriendService.java
@@ -2,32 +2,44 @@ package com.cocotalk.user.service;
 
 import com.cocotalk.user.domain.entity.Friend;
 import com.cocotalk.user.domain.entity.User;
+import com.cocotalk.user.domain.vo.FriendVo;
+import com.cocotalk.user.domain.vo.UserVo;
+import com.cocotalk.user.dto.exception.GlobalError;
+import com.cocotalk.user.dto.exception.GlobalException;
 import com.cocotalk.user.dto.request.FriendAddRequest;
-import com.cocotalk.user.dto.response.FriendResponse;
-import com.cocotalk.user.mapper.FriendMapper;
+import com.cocotalk.user.utils.mapper.FriendMapper;
+import com.cocotalk.user.utils.mapper.UserMapper;
 import com.cocotalk.user.repository.FriendRepository;
 import com.cocotalk.user.repository.UserRepository;
-import com.cocotalk.user.support.GlobalError;
-import com.cocotalk.user.support.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FriendService {
-    private final UserRepository userRepository;
     private final FriendRepository friendRepository;
+    private final UserRepository userRepository;
     private final FriendMapper friendMapper;
+    private final UserMapper userMapper;
 
-    public FriendResponse add(FriendAddRequest request){
-        User fromUser = userRepository.findById(request.getFromUid())
-                .orElseThrow(() -> new GlobalException(GlobalError.USER_NOT_EXIST));
-        User toUser =  userRepository.findById(request.getToUid())
-                .orElseThrow(() -> new GlobalException(GlobalError.USER_NOT_EXIST));
+    public static final GlobalException INVALID_ID = new GlobalException(GlobalError.BAD_REQUEST, "해당 id를 갖는 유저가 존재하지 않습니다.");
+
+    public FriendVo add(User fromUser, FriendAddRequest request){
+        User toUser =  userRepository.findById(request.getToUid()).orElseThrow(() -> INVALID_ID);
         Friend friend = friendRepository.save(Friend.builder()
                 .fromUser(fromUser)
                 .toUser(toUser)
                 .build());
-        return friendMapper.toDto(friend);
+        return friendMapper.toVo(friend);
+    }
+
+    public List<UserVo> find(User fromUser) {
+        List<Friend> friends = friendRepository.findByFromUserId(fromUser.getId());
+        return friends.stream()
+                .map(friend -> userMapper.toVo(friend.getToUser()))
+                .collect(Collectors.toList());
     }
 }
