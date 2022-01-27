e424a9e2e8c6c771d007edc6074e52d4735078e8
황종훈 <ybell1028@gmail.com>
2022-01-27 11:30:16 +0900

    feat : Enable ServiceDiscovery

--- Cocotalk-Discovery/src/main/java/com/cocotalk/discovery/DiscoveryApplication.java
+++ Cocotalk-Discovery/src/main/java/com/cocotalk/discovery/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.cocotalk.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
