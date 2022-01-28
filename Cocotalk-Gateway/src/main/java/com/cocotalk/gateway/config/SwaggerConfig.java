cc28883614b3ad835b7402ac418d2fd6e44acf77
ybell1028 <ybell1028@gmail.com>
2022-01-29 00:24:34 +0900

    feat : MSA 통합 springdoc 설정

---
+++ Cocotalk-Gateway/src/main/java/com/cocotalk/gateway/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.cocotalk.gateway.config;
+
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public CommandLineRunner openApiGroups(
+            RouteDefinitionLocator locator,
+            SwaggerUiConfigParameters swaggerUiParameters) {
+        return args -> locator
+                .getRouteDefinitions().collectList().block()
+                .stream()
+                .map(RouteDefinition::getId)
+                .filter(id -> id.matches(".*-service"))
+                .map(id -> id.replace("-service", ""))
+                .forEach(swaggerUiParameters::addGroup);
+    }
+}
