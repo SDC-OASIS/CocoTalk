ff469accb004d9ef25db3941822872ffdb7b6726
황종훈 <ybell1028@gmail.com>
2022-01-29 15:10:27 +0900

    feat : springdoc SecurityScheme(Authorize button) 추가

---
+++ src/main/java/com/cocotalk/user/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.cocotalk.user.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "Documentation User API v1.0"))
+@SecurityScheme(name = "X-ACCESS-TOKEN", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+public class SwaggerConfig {
+}
