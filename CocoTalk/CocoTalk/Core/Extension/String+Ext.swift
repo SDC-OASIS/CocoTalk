965d313059c5ddbd176d1a8dd308ea898f75f33d
bengHak <qkwl4678@naver.com>
2022-02-03 22:34:58 +0900

    feat - String extension 추가 (전화번호 정규식)

---
+++ CocoTalk/CocoTalk/Core/Extension/String+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  String+Ext.swift
+//  CocoTalk
+//
+//  Created by byunghak on 2022/02/03.
+//
+
+import Foundation
+
+extension String {
+    func pretty() -> String {
+        let _str = self.trimmingCharacters(in: .whitespacesAndNewlines)
+        if let regex = try? NSRegularExpression(pattern: "([0-9]{3})([0-9]{3,4})([0-9]{4})", options: .caseInsensitive) {
+            let modString = regex.stringByReplacingMatches(in: _str, options: [], range: NSRange(_str.startIndex..., in: _str), withTemplate: "$1-$2-$3")
+            return modString
+        }
+        return self
+    }
+}
