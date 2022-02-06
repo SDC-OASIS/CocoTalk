7083d3c61e59ede4bc7c17fc1a924d28030f5311
co323co <co323co@gmail.com>
2022-02-06 20:01:06 +0900

    feat: Add listenMessage sample code

--- src/main/java/com/cocotalk/push/controller/KafkaController.java
+++ src/main/java/com/cocotalk/push/controller/KafkaController.java
@@ -1,18 +1,30 @@
 package com.cocotalk.push.controller;
 
+import com.cocotalk.push.service.KafkaConsumer;
 import com.cocotalk.push.service.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/kafka")
 public class KafkaController {
     private final KafkaProducer producer;
+    private final KafkaConsumer consumer;
+
+
+    @GetMapping
+    public String listenMessage(@RequestParam("message") String message) {
+        try {
+            this.consumer.consume(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
 
     @PostMapping
     public String sendMessage(@RequestParam("message") String message) {
