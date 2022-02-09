29d91ff3d63bd2089260464fed667094ea4e6bd9
ybell1028 <ybell1028@gmail.com>
2022-02-09 13:30:56 +0900

    Initial Commit

---
+++ src/test/java/com/cocotalk/presence/PresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocotalk.presence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
