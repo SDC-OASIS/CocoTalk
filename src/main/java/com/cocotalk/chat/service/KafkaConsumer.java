5fc0f92b886043dde2ef703066aa055a40bda039
co323co <co323co@gmail.com>
2022-02-11 23:37:43 +0900

    feat: Update title as push message

--- src/main/java/com/cocotalk/chat/service/KafkaConsumer.java
+++ src/main/java/com/cocotalk/chat/service/KafkaConsumer.java
@@ -19,19 +19,6 @@ public class KafkaConsumer {
     private final ObjectMapper mapper;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-//    @KafkaListener(topics = "${spring.kafka.chat-topic}")
-//    public void consume(String message) {
-//        try{
-//            ChatTopicDto chatTopicDto = mapper.readValue(message, ChatTopicDto.class);
-//            //websocket 토픽에 메시지 전송
-//            log.info(String.format("[Parse] Consumed message : %s", message));
-//            simpMessagingTemplate.convertAndSend("/topic/" + chatTopicDto.getRoomId() + "/message", chatTopicDto.getMessageVo());
-//        }catch (Exception e){
-//            log.error("[consume] : json 파싱 실패");
-//            e.printStackTrace();
-//        }
-//    }
-
     @KafkaListener(topics = "${spring.kafka.chat-topic}")
     public void consume(String message) {
         try{
