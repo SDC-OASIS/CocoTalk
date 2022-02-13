2a29ea732b8155c9937e050c2147d9fc8ecb310a
co323co <co323co@gmail.com>
2022-02-13 20:09:14 +0900

    feat: Update Response and ResponseStatus

--- src/main/java/com/cocotalk/push/dto/common/response/Response.java
+++ src/main/java/com/cocotalk/push/dto/common/response/Response.java
@@ -1,7 +1,6 @@
 package com.cocotalk.push.dto.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,15 +9,21 @@ import lombok.Setter;
 
 import java.util.Date;
 
+
+/**
+ *
+ * 요청이 성공인 경우 code는 포함되지 않습니다.
+ * 요청이 실패한 경우 result는 포함되지 않습니다.
+ *
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 @JsonPropertyOrder({"isSuccess", "status", "code", "message", "result", "timestamp"})
 public class Response<T> {
-    @JsonProperty(value = "isSuccess")
-    private boolean isSuccess;
-    private int status;
-    private int code;
+    private Boolean isSuccess;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer code;
     private String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
@@ -29,8 +34,6 @@ public class Response<T> {
      */
     public Response(T result, ResponseStatus status) {
         this.isSuccess = true;
-        this.status = status.getStatus();
-        this.code = status.getCode();
         this.message = status.getMessage();
         this.result = result;
         this.timestamp = new Date();
@@ -41,7 +44,6 @@ public class Response<T> {
      */
     public Response(ResponseStatus status) {
         this.isSuccess = false;
-        this.status = status.getStatus();
         this.code = status.getCode();
         this.message = status.getMessage();
         this.timestamp = new Date();
