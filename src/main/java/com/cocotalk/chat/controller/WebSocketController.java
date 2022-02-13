ee4bc99d84b8059ac68b293ea3a84b6fdc87a6a4
co323co <co323co@gmail.com>
2022-02-13 16:51:59 +0900

    feat: Add swagger description on crash api

--- src/main/java/com/cocotalk/chat/controller/WebSocketController.java
+++ src/main/java/com/cocotalk/chat/controller/WebSocketController.java
@@ -3,6 +3,8 @@ package com.cocotalk.chat.controller;
 import com.cocotalk.chat.dto.request.CrashRequest;
 import com.cocotalk.chat.service.kafka.KafkaProducer;
 import com.cocotalk.chat.utils.WebSocketUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "소켓 전송 API")
 public class WebSocketController {
-    private final WebSocketUtils websocketUtil;
     private final KafkaProducer kafkaProducer;
 
     @PostMapping("/crash")
+    @Operation(summary = "요청의 userId의 clientType이 일치하는 접속중인 기기 중 fcm token이 다른 기기 전부 로그아웃 요청")
     public void crash(@RequestBody CrashRequest request) {
         String type = request.getClientType().toLowerCase();
         if("mobile".equals(type) || "web".equals(type)){
