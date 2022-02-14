0c563226618fe56490700a2c8496fc14013b664f
ybell1028 <ybell1028@gmail.com>
2022-02-14 23:32:04 +0900

    feat : 프리젠스 서버 Down시 재연결 로직 추가, WebSocket 통신 로직 WebSocketUtil에 통합

--- src/main/java/com/cocotalk/chat/utils/WebSocketUtil.java
+++ src/main/java/com/cocotalk/chat/utils/WebSocketUtil.java
@@ -1,27 +1,43 @@
 package com.cocotalk.chat.utils;
 
+import com.cocotalk.chat.dto.request.PresenceRequest;
+import com.cocotalk.chat.exception.CustomError;
+import com.cocotalk.chat.exception.CustomException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
 import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 
 import java.net.URI;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
-public class WebSocketUtil extends WebSocketClient {
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    // private static ScheduledFuture<?> task;
+@Getter
+public class WebSocketUtil extends WebSocketClient implements ApplicationListener<ContextClosedEvent> {
+    private final String stompEndpoint;
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+    private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private static boolean reconnect = false;
 
-    public WebSocketUtil(URI serverUri, Draft protocolDraft) {
+    public WebSocketUtil(URI serverUri, Draft protocolDraft, String stompEndpoint) {
         super(serverUri, protocolDraft);
+        this.stompEndpoint = stompEndpoint;
     }
 
     @Override
     public void onOpen(ServerHandshake handshake) {
         log.info("Opened connection to presence service");
-        // task.cancel(false);
+        registerServer();
+        if(reconnect) {
+            executor.shutdownNow();
+            reconnect = false;
+        }
     }
 
     @Override
@@ -32,7 +48,15 @@ public class WebSocketUtil extends WebSocketClient {
     @Override
     public void onClose(int code, String reason, boolean remote) {
         log.info("Closed connection to presence service");
-        // tryReconnect();
+        executor = Executors.newSingleThreadScheduledExecutor();
+        reconnect = true;
+        tryReconnect();
+    }
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        log.info("Withdraw server endpoint to Presence server");
+        withdrawServer();
     }
 
     @Override
@@ -40,14 +64,46 @@ public class WebSocketUtil extends WebSocketClient {
         e.printStackTrace();
     }
 
-//    public void tryReconnect() {
-//         task = executor.scheduleAtFixedRate(() -> {
-//            try {
-//                log.info("Try reconnect per 5 seconds");
-//                this.reconnectBlocking();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }, 0, 50000, TimeUnit.MILLISECONDS);
-//    }
+    public void tryReconnect() {
+        executor.schedule(() -> {
+            try {
+                log.info("Try reconnect every 10 seconds");
+                this.reconnectBlocking();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, 10, TimeUnit.SECONDS);
+    }
+
+    public void registerServer() {
+        PresenceRequest registerRequest = PresenceRequest.builder()
+                .action("register")
+                .serverUrl(stompEndpoint)
+                .build();
+        try {
+            String requestString = objectMapper.writeValueAsString(registerRequest);
+            log.info(requestString);
+            this.send(requestString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("[PresenceService/run] : 프리젠스 서버에게 URL 등록 메시지를 보내는 도중 문제가 발생했습니다.");
+            throw new CustomException(CustomError.JSON_PARSE, e);
+        }
+    }
+
+    public void withdrawServer() { // 서버 종료 시
+        PresenceRequest withdrawRequest = PresenceRequest.builder()
+                .action("withdraw")
+                .serverUrl(stompEndpoint)
+                .build();
+        try {
+            String requestString = objectMapper.writeValueAsString(withdrawRequest);
+            log.info(requestString);
+            this.send(requestString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("[PresenceService/onApplicationEvent] : 프리젠스 서버에게 URL 해제 메시지를 보내는 도중 문제가 발생했습니다.");
+            throw new CustomException(CustomError.JSON_PARSE, e);
+        }
+    }
 }
